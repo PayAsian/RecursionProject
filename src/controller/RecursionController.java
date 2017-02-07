2e0adaed2482f220c9f4b78b9608fc0ef62558be
PayAsian <ptre8426@740s-pl-102.local>
2017-02-07 12:43:40 -0700

    Added code to the controller

--- src/controller/RecursionController.java
+++ src/controller/RecursionController.java
@@ -1,9 +1,57 @@
 package controller;
 
+import javax.swing.JOptionPane;
+
+import model.RecursionTool;
+import view.RecursionFrame;
+
 public class RecursionController 
 {
 public void start()
 {
+	JOptionPane.showMessageDialog(baseFrame, "look we are doing math");
+}
+
+private RecursionFrame baseFrame;
+private RecursionTool mathTool;
+
+public RecursionController()
+{
+	this.mathTool = new RecursionTool();
+	this.baseFrame = new RecursionFrame(this);
 	
+}
+
+public String transerFactorial(String input)
+{
+	String factorialInfo = "The factorial of " + input + " is ";
+	
+	if(isValid(input))
+	{
+		factorialInfo += mathTool.calculateFactorial(Integer.parseInt(input));
+	}
+	
+	return factorialInfo;
+}
+
+private boolean isValid(String testValue)
+{
+	try
+	{
+		int test = Integer.parseInt(testValue);
+		if(test >= 0)
+		{
+		return true;	
+		}
+		else
+		{
+			return false;
+		}
+	}
+	catch(NumberFormatException error)
+	{
+		JOptionPane.showMessageDialog(baseFrame, "Type in a valic number >= 0");
+		return false;
+	}
 }
 }
